5db3e72bd9bf53af3c8b6837778c1c24f86de409
vincenteeeei <vicenteobiar@gmail.com>
2025-01-16 13:07:30 +0800

    header 100%

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const burgir = document.querySelector(".toggle__menu");
+const headerNav = document.querySelector(".header .header__nav");
 burgir.addEventListener("click", () => {
 burgir.classList.toggle("open");
+headerNav.classList.toggle("open");
 });
