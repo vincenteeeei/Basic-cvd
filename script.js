2ed140e4dcabedabc9385db18be373baf59213fd
vincenteeeei <vicenteobiar@gmail.com>
2025-01-23 14:46:25 +0800

    contact 100%

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ burgir.addEventListener("click", () => {
 burgir.classList.toggle("open");
 headerNav.classList.toggle("open");
 });
+
+const b2Top = document.querySelector(".backToTop");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 300) {
+    b2Top.classList.add("open");
+  } else {
+    b2Top.classList.remove("open");
+  }
+});
